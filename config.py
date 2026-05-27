c334cbbb5bd3e232251901890063e0eeb2bd16a7
FusionBots26 <gagansiwan2007@hotmail.com>
2026-05-27 20:11:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-BASE_API_URL = getenv("BASE_API_URL", "https://zyro.zyronetworks.shop")
+BASE_API_URL = getenv("BASE_API_URL", "https://tgapi.xbitcode.com")
 BASE_API_KEY = getenv("BASE_API_KEY", "")
 
 # Get your token from @BotFather on Telegram.
